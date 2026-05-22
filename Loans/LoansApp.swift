38f25becda741d75fa4866c6dcbed03e8f10cec2
divin3circle <sylus.abel@icloud.com>
2026-05-22 13:09:24 +0300

    Initial Commit

---
+++ Loans/LoansApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoansApp.swift
+//  Loans
+//
+//  Created by Sylus Abel on 22/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct LoansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
